ed0a497170481f23e6919ca0474942691b9dace6
markmester <mmester6016@gmail.com>
2017-11-25 18:39:29 -0500

    idk

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import logging
+import time
 
 from src.utilities import get_config, setup_logging
 from src.ARDrone import ARDrone
+
+
 setup_logging()
 ARdrone_logger = logging.getLogger("ARdrone-controller")
 
@@ -12,3 +15,9 @@ if __name__ == '__main__':
     logging.info("Starting drone test...")  # root logger
     drone = ARDrone(drone_config=config)
     drone.command(config['commands']['AT_REF']['takeoff'], "1")
+
+    time.sleep(5)
+
+    for i in range(1, 10):
+        drone.command(config['commands']['AT_REF']['land'], "1")
+        time.sleep(.5)
